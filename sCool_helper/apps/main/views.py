03833126309e4cdc28220cb770d732a6e8215ecd
Naruto-sys <gavriil.belov@yandex.ru>
2020-03-15 16:22:05 +0300

    last_commit

--- sCool_helper/apps/main/views.py
+++ sCool_helper/apps/main/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.core.cache import cache
 
 
 def main(request):
     context = {"login": cache.get("login")}
+
     return render(request, r'main\main.html', context)
